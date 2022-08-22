ab5d8ddc44185f60482ba6a546d68b7119a03ace
William Hart <hart.wl@gmail.com>
2022-08-22 21:04:09 +1000

    Make market a table
    Its not great, but I dunno what else to do rn

--- src/game/ui/ship_respawn_bar.rs
+++ src/game/ui/ship_respawn_bar.rs
@@ -6,6 +6,7 @@ use crate::{
         ShipRespawnBar, BOX_TYPES, DESTINATIONS,
     },
     loader::{FontAssets, TextureAssets},
+    GRID_SIZE,
 };
 
 use super::launch_ships::spawn_ship_buttons;
@@ -99,62 +100,130 @@ pub fn spawn_ship_respawn_bar(
             layout
                 .spawn_bundle(NodeBundle {
                     style: Style {
-                        size: Size::new(Val::Percent(100.0), Val::Px(48.0)),
-                        padding: UiRect::all(Val::Px(10.0)),
-                        align_items: AlignItems::Center,
+                        size: Size::new(Val::Px(11. * GRID_SIZE), Val::Px(4.0 * GRID_SIZE)),
+                        justify_content: JustifyContent::FlexStart,
+                        align_items: AlignItems::FlexEnd,
+                        flex_direction: FlexDirection::ColumnReverse,
                         ..default()
                     },
                     color: Color::rgba(0.15, 0.15, 0.15, 0.35).into(),
                     ..Default::default()
                 })
-                .with_children(|market_layout| {
-                    DESTINATIONS.iter().for_each(|destination| {
-                        market_layout.spawn_bundle(TextBundle {
-                            text: Text::from_section(
-                                format!("{}", destination),
-                                small_text_style.clone(),
-                            ),
+                .with_children(|market_table| {
+                    // table row
+                    market_table
+                        .spawn_bundle(NodeBundle {
+                            style: Style {
+                                size: Size::new(Val::Percent(100.), Val::Px(GRID_SIZE)),
+                                justify_content: JustifyContent::Center,
+                                align_items: AlignItems::FlexEnd,
+                                ..default()
+                            },
+                            color: Color::NONE.into(),
                             ..default()
+                        })
+                        .with_children(|parent_row| {
+                            parent_row.spawn_bundle(NodeBundle {
+                                style: Style {
+                                    size: Size::new(Val::Px(3.0 * GRID_SIZE), Val::Auto),
+                                    ..default()
+                                },
+                                color: Color::NONE.into(),
+                                ..default()
+                            });
+
+                            BOX_TYPES.iter().for_each(|bt| {
+                                parent_row
+                                    .spawn_bundle(NodeBundle {
+                                        style: Style {
+                                            size: Size::new(
+                                                Val::Px(2.0 * GRID_SIZE),
+                                                Val::Px(GRID_SIZE),
+                                            ),
+                                            justify_content: JustifyContent::Center,
+                                            align_items: AlignItems::Center,
+                                            ..default()
+                                        },
+                                        color: Color::NONE.into(),
+                                        ..default()
+                                    })
+                                    .with_children(|nb| {
+                                        nb.spawn_bundle(ImageBundle {
+                                            image: bt.get_image(&textures).into(),
+                                            transform: Transform::from_scale(Vec3::splat(0.9)),
+                                            ..default()
+                                        });
+                                    });
+                            });
                         });
 
-                        BOX_TYPES.iter().for_each(|bt| {
-                            market_layout
-                                .spawn_bundle(NodeBundle {
+                    // draw the market rows
+                    DESTINATIONS.iter().for_each(|destination| {
+                        market_table
+                            .spawn_bundle(NodeBundle {
+                                style: Style {
+                                    size: Size::new(Val::Percent(100.), Val::Px(GRID_SIZE)),
+                                    justify_content: JustifyContent::Center,
+                                    align_items: AlignItems::FlexEnd,
+                                    ..default()
+                                },
+                                color: Color::NONE.into(),
+                                ..default()
+                            })
+                            .with_children(|market_row| {
+                                market_row.spawn_bundle(TextBundle {
+                                    text: Text::from_section(
+                                        format!("{}", destination),
+                                        small_text_style.clone(),
+                                    ),
                                     style: Style {
-                                        margin: UiRect::new(
-                                            Val::Undefined,
-                                            Val::Px(5.0),
-                                            Val::Undefined,
-                                            Val::Undefined,
-                                        ),
-                                        align_items: AlignItems::Center,
+                                        size: Size::new(Val::Px(3.0 * GRID_SIZE), Val::Auto),
                                         ..default()
                                     },
-                                    color: Color::NONE.into(),
                                     ..default()
-                                })
-                                .insert(MarketPriceIndicator(destination.clone(), bt.clone()))
-                                .with_children(|market_item| {
-                                    market_item.spawn_bundle(ImageBundle {
-                                        image: bt.get_image(&textures).into(),
-                                        transform: Transform::from_scale(Vec3::splat(0.7)),
-                                        ..default()
-                                    });
-                                    market_item
-                                        .spawn_bundle(TextBundle {
-                                            text: Text::from_section("0", small_text_style.clone()),
-                                            ..default()
-                                        })
-                                        .insert(MarketPriceValueIndicator);
-                                    market_item
-                                        .spawn_bundle(ImageBundle {
-                                            image: textures.up.clone().into(),
-                                            transform: Transform::from_scale(Vec3::splat(0.7)),
-                                            ..default()
-                                        })
-                                        .insert(MarketPriceDirectionIndicator);
                                 });
-                        });
+
+                                BOX_TYPES.iter().for_each(|bt| {
+                                    market_row
+                                        .spawn_bundle(NodeBundle {
+                                            style: Style {
+                                                size: Size::new(
+                                                    Val::Px(2.0 * GRID_SIZE),
+                                                    Val::Auto,
+                                                ),
+                                                justify_content: JustifyContent::Center,
+                                                align_items: AlignItems::Center,
+                                                ..default()
+                                            },
+                                            color: Color::NONE.into(),
+                                            ..default()
+                                        })
+                                        .insert(MarketPriceIndicator(
+                                            destination.clone(),
+                                            bt.clone(),
+                                        ))
+                                        .with_children(|market_item| {
+                                            market_item
+                                                .spawn_bundle(TextBundle {
+                                                    text: Text::from_section(
+                                                        "0",
+                                                        small_text_style.clone(),
+                                                    ),
+                                                    ..default()
+                                                })
+                                                .insert(MarketPriceValueIndicator);
+                                            market_item
+                                                .spawn_bundle(ImageBundle {
+                                                    image: textures.up.clone().into(),
+                                                    transform: Transform::from_scale(Vec3::splat(
+                                                        0.7,
+                                                    )),
+                                                    ..default()
+                                                })
+                                                .insert(MarketPriceDirectionIndicator);
+                                        });
+                                });
+                            });
                     });
                 });
 
