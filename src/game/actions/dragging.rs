9064c189209468b28ead45f36e88bc5711544979
William Hart <hart.wl@gmail.com>
2022-08-26 08:28:55 +1000

    Allow picking up physics crates

--- src/game/actions/dragging.rs
+++ src/game/actions/dragging.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 
 use crate::{
-    game::components::{BoxType, Cart, CartCrate, FollowMouse},
+    game::components::{BoxType, Cart, CartCrate, FollowMouse, PhysicsCrate},
     input::MousePosition,
     loader::TextureAssets,
 };
@@ -12,8 +12,13 @@ pub struct DraggingBox {
     pub is_front_slot: bool,
 }
 
+pub enum StartDraggingItem {
+    Cart(Entity),
+    PhysicsCrate(Entity),
+}
+
 pub struct OnStartDragging {
-    pub cart_entity: Entity,
+    pub dragged_entity: StartDraggingItem,
     pub is_front_slot: bool,
 }
 
@@ -24,27 +29,52 @@ pub fn start_dragging(
     mut events: EventReader<OnStartDragging>,
     mut carts: Query<(&mut Cart, &Children)>,
     mut child_crates: Query<(&mut Visibility, &CartCrate)>,
+    physics_crates: Query<&PhysicsCrate>,
 ) {
     for event in events.iter() {
-        // find the cart that we're dragging from
-        let (mut cart, children) = match carts.get_mut(event.cart_entity) {
-            Ok(cart) => cart,
-            Err(e) => {
-                warn!("{:?}", e);
-                continue;
-            }
-        };
+        match event.dragged_entity {
+            StartDraggingItem::Cart(cart_ent) => {
+                let (mut cart, children) = match carts.get_mut(cart_ent) {
+                    Ok(cart) => cart,
+                    Err(e) => {
+                        warn!("get cart entity: {:?}", e);
+                        continue;
+                    }
+                };
 
-        // update the cart and set the dragging item here
-        dragging.box_type = if event.is_front_slot {
-            let item = cart.front;
-            cart.front = None;
-            item
-        } else {
-            let item = cart.back;
-            cart.back = None;
-            item
-        };
+                // update the cart and set the dragging item here
+                dragging.box_type = if event.is_front_slot {
+                    let item = cart.front;
+                    cart.front = None;
+                    item
+                } else {
+                    let item = cart.back;
+                    cart.back = None;
+                    item
+                };
+
+                // hide the box that's being dragged
+                for child in children.iter() {
+                    let (mut vis, child_crate) =
+                        child_crates.get_mut(*child).expect("should have children");
+                    if child_crate.is_front_slot == event.is_front_slot {
+                        vis.is_visible = false;
+                    }
+                }
+            }
+            StartDraggingItem::PhysicsCrate(crate_ent) => {
+                let physics_crate = match physics_crates.get(crate_ent) {
+                    Ok(pc) => pc,
+                    Err(e) => {
+                        warn!("get physics crate: {:?}", e);
+                        continue;
+                    }
+                };
+
+                dragging.box_type = Some(physics_crate.box_type);
+                commands.entity(crate_ent).despawn_recursive();
+            }
+        }
 
         if dragging.box_type.is_none() {
             // nothing to drag
@@ -52,15 +82,6 @@ pub fn start_dragging(
         }
 
         dragging.is_front_slot = event.is_front_slot;
-
-        // hide the box that's being dragged
-        for child in children.iter() {
-            let (mut vis, child_crate) =
-                child_crates.get_mut(*child).expect("should have children");
-            if child_crate.is_front_slot == event.is_front_slot {
-                vis.is_visible = false;
-            }
-        }
 
         // spawn a box based on the box type and attach it to the mouse
         dragging.box_entity = Some(
--- src/game/actions/pickup.rs
+++ src/game/actions/pickup.rs
@@ -2,7 +2,10 @@ use bevy::prelude::*;
 use leafwing_input_manager::prelude::ActionState;
 
 use crate::{
-    game::{actions::CART_SPRITE_HALF_WIDTH, components::Cart},
+    game::{
+        actions::{dragging::StartDraggingItem, CART_SPRITE_HALF_WIDTH},
+        components::{Cart, PhysicsCrate},
+    },
     input::{MousePosition, PlayerActions},
     GRID_SIZE,
 };
@@ -19,23 +22,21 @@ pub fn click_to_pickup(
     mut start_events: EventWriter<OnStartDragging>,
     mut drop_events: EventWriter<OnDropCrate>,
     action_state_query: Query<&ActionState<PlayerActions>>,
-    mut carts: Query<(Entity, &Transform), With<Cart>>,
+    carts: Query<(Entity, &Transform), With<Cart>>,
+    boxes: Query<(Entity, &Transform), With<PhysicsCrate>>,
 ) {
     let action_state = action_state_query.single();
     let x = mouse_pos.world.x;
     let y = mouse_pos.world.y;
 
     if action_state.just_pressed(PlayerActions::Click) {
-        if !(CART_MIN_Y..=CART_MAX_Y).contains(&y) {
-            return;
-        }
-
-        for (cart_ent, cart_tx) in carts.iter_mut() {
+        for (cart_ent, cart_tx) in carts.iter() {
             let delta = x - cart_tx.translation.x;
             // carts are 160px wide, the last two grid squares (32px) are for boxes.
             // be a bit flexible with the clicking (i.e. doen't require directly on the sprite)
             if !((CART_SPRITE_HALF_WIDTH - 2.0 * GRID_SIZE)..=CART_SPRITE_HALF_WIDTH)
                 .contains(&delta)
+                || !(CART_MIN_Y..=CART_MAX_Y).contains(&y)
             {
                 continue;
             }
@@ -44,15 +45,48 @@ pub fn click_to_pickup(
                 (((x - cart_tx.translation.x) - (GRID_SIZE / 2.0)) / GRID_SIZE).floor() as u32;
 
             info!(
-                "Clicked box {} on cart {:?} at {}",
-                zone, cart_ent, cart_tx.translation
+                "Picking up box {} on cart {:?} at {} in the {} zone",
+                zone,
+                cart_ent,
+                cart_tx.translation,
+                if zone == 0 { "front" } else { "back" }
             );
 
             // trigger the event to handle drag start
             start_events.send(OnStartDragging {
-                cart_entity: cart_ent,
+                dragged_entity: StartDraggingItem::Cart(cart_ent),
                 is_front_slot: zone == 0,
             });
+
+            return;
+        }
+
+        print!(".");
+
+        // go through the physics boxes and see if we should pick one up
+        // this test is a bit simpler because we can just check x/y bounds
+        for (box_ent, box_tx) in boxes.iter() {
+            let in_x = ((box_tx.translation.x - GRID_SIZE / 2.0)
+                ..(box_tx.translation.x + GRID_SIZE / 2.0))
+                .contains(&x);
+            let in_y = ((box_tx.translation.y - GRID_SIZE / 2.0)
+                ..(box_tx.translation.y + GRID_SIZE / 2.0))
+                .contains(&y);
+            println!("{} - {}", in_x, in_y);
+
+            if in_x && in_y {
+                info!(
+                    "Picking up physics crate {:?} from the warehouse at {}",
+                    box_ent, box_tx.translation
+                );
+
+                // trigger the event to handle drag start
+                start_events.send(OnStartDragging {
+                    dragged_entity: StartDraggingItem::PhysicsCrate(box_ent),
+                    is_front_slot: true,
+                });
+                return;
+            }
         }
     } else if dragging.box_entity.is_some() && action_state.just_released(PlayerActions::Click) {
         drop_events.send(OnDropCrate);
