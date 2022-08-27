e9ed836c1865d3caae20e4a7ba0a66f3b04349ee
William Hart <hart.wl@gmail.com>
2022-08-27 14:46:55 +1000

    Random confusing fix for sprite not rendering

--- src/loader.rs
+++ src/loader.rs
@@ -118,6 +118,9 @@ pub struct TextureAssets {
     #[asset(path = "textures/box_type_wine.png")]
     pub box_type_wine: Handle<Image>,
 
+    #[asset(path = "textures/box_type_wine2.png")]
+    pub box_type_wine2: Handle<Image>,
+
     #[asset(texture_atlas(tile_size_x = 288., tile_size_y = 224., columns = 4, rows = 1))]
     #[asset(path = "textures/ship.png")]
     pub ship: Handle<TextureAtlas>,
